dcee0c2355a627f27b33fe07aac2a92466b3b635
Chris Lopes <chrislopes3667@gmail.com>
2025-03-19 02:33:42 +0530

    Update file content

--- test.sol
+++ test.sol
@@ -3,8 +3,19 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
     uint256 public myVariable;
+    address public owner;
 
-    function setMyVariable(uint256 _newValue) public {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function setMyVariable(uint256 _newValue) public onlyOwner {
+        require(_newValue <= type(uint256).max - 1000, "Value too large");
         myVariable = _newValue;
     }
 
