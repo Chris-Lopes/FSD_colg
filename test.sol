668e7141ab8a9e6c08e01841cd755a856b4db0ea
Chris Lopes <chrislopes3667@gmail.com>
2025-03-19 02:38:50 +0530

    Update file content

--- test.sol
+++ test.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 contract MyContract {
     uint256 public myVariable;
     address public owner;
+    uint256 private constant minValue = 0; //Example minimum value
 
     constructor() {
         owner = msg.sender;
@@ -15,7 +16,8 @@ contract MyContract {
     }
 
     function setMyVariable(uint256 _newValue) public onlyOwner {
-        require(_newValue <= type(uint256).max - 1000, "Value too large");
+        require(_newValue >= minValue, "Value too small");
+        require(type(uint256).max - _newValue >= 1000, "Value too large");
         myVariable = _newValue;
     }
 
