7462ecebc2b1cffcd005f6084ba60a341c1743a1
Chris Lopes <chrislopes3667@gmail.com>
2025-03-19 02:33:11 +0530

    Update file content

--- test.sol
+++ test.sol
@@ -1,1 +1,14 @@
-hi
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint256 public myVariable;
+
+    function setMyVariable(uint256 _newValue) public {
+        myVariable = _newValue;
+    }
+
+    function getMyVariable() public view returns (uint256) {
+        return myVariable;
+    }
+}
